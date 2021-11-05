113f508918a761a202c1430384325d7791ee6440
Lorina Poland <lorinapoland@gmail.com>
2021-11-05 12:55:17 -0700

    cleaned up and commented

--- connect-astra.mjs
+++ connect-astra.mjs
@@ -5,61 +5,79 @@ import { StargateClient, StargateBearerToken, Query, Batch, BatchQuery, Response
 
 try {
 
-    //const bearer_token = "AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b";
-    // const bearer_token = "AstraCS:qTvPRvZTLygyfEZynXIRcOrs:cbd02f55e76e6e948673e8127bbf81feeb6a5564ccfe1f513cf73b9409cf70ed";
+    // Astra DB configuration
+    const astra_uri = "0ba933af-90c2-46e9-887a-a387ba75411b-westus2.apps.astra-dev.datastax.com:443";
     const bearer_token = "AstraCS:YUskXkABjYztQJfTHNkOGHOI:09fa16889d7217b23b3854d3b2858a17b895ec272494a1539e47200b87567f26";
 
-    //const astra_uri = "a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com:443";
-    // const astra_uri = "ff1d03c6-aaae-440c-b463-4508b2993d0f-us-east1.apps.astra-dev.datastax.com:443";
-    const astra_uri = "0ba933af-90c2-46e9-887a-a387ba75411b-westus2.apps.astra-dev.datastax.com:443";
-
-    // Enter a bearer token for Astra
+    // Set up the authenication 
+    // For Astra DB: Enter a bearer token for Astra, downloaded from the Astra DB dashboard
     const bearerToken = new StargateBearerToken(bearer_token);
     const credentials = grpc.credentials.combineChannelCredentials(grpc.credentials.createSsl(), bearerToken);
-    //console.log(credentials);
+    
+    // console.log(credentials);
 
-    // Create the gRPC client, passing it the address of the gRPC endpoint
+    // Create the gRPC client
+    // For Astra DB: passing the credentials created above
     const stargateClient = new StargateClient(astra_uri, credentials);
+    
     console.log("made client");
 
     // Create a promisified version of the client, so we don't need to use callbacks
     const promisifiedClient = promisifyStargateClient(stargateClient);
-    console.log("promisified client")
     
-    // Create a new table
-    const createTableStatement = new Query();
-    createTableStatement.setCql("CREATE TABLE IF NOT EXISTS test.users (firstname text PRIMARY KEY,lastname text);");
+    console.log("promisified client")
 
-    await promisifiedClient.executeQuery(createTableStatement);
-    // INSERT two rows/records
+    // For Astra DB:  create  a keyspace in the Astra DB dashboard
+
+    // For Astra DB: Create a new table
+    const createTableStatement = new Query();
+    // Set the CQL statement
+    createTableStatement.setCql("CREATE TABLE IF NOT EXISTS test.users (key text PRIMARY KEY,value text);");
+
+    await promisifiedClient.executeQuery(
+      createTableStatement
+    );
+
+    console.log("created table");
+
+    // For Astra DB: INSERT two rows/records
+    // Create two queries that will be run in a batch statement
     const insertOne = new BatchQuery();
     const insertTwo = new BatchQuery();
 
+    // Set the CQL statement
     insertOne.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Lorina', 'Poland')`);
     insertTwo.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Doug', 'Wettlaufer')`);
 
-    // Execute the batch
+    // Define the new batch to include the 2 insertions
     const batch = new Batch();
     batch.setQueriesList([insertOne, insertTwo]);
 
+    // For Astra DB: execute the batch statement 
     const batchResult = await promisifiedClient.executeBatch(
       batch
     );
     console.log("inserted data");
 
-    const queryString = 'SELECT firstname, lastname FROM test.users;'
+    // For Astra DB: SELECT the data to read from the table
     const query = new Query();
+    const queryString = 'SELECT firstname, lastname FROM test.users;'
+    // Set the CQL statement using the string defined in the last line
     query.setCql(queryString);
-    //console.log(queryString);
 
+    // For  Astra DB: execute the query statement
     const response = await promisifiedClient.executeQuery(
       query
     );
+
     console.log("select executed")
 
+    // Get the results from the execute query statement
+    // and separate into an array to print out the results
     const resultSet = response.getResultSet();
     const rows = resultSet.getRowsList();
 
+    // This for loop gets 2 results
     for ( let i = 0; i < 2; i++) {
       var valueToPrint = "";
       for ( let j = 0; j < 2; j++) {
@@ -69,7 +87,11 @@ try {
       }
       console.log(valueToPrint);
     }
+
+    console.log("everything worked!")
   } catch (e) {
+    // Print out any errors that occur while running this script
     console.log(e);
   }
+
 
--- connect.mjs
+++ connect.mjs
@@ -1,82 +1,111 @@
 #!/usr/bin/env zx
 
 import * as grpc from "@grpc/grpc-js";
-import { StargateClient, StargateTableBasedToken, Query, Response, promisifyStargateClient } from "@stargate-oss/stargate-grpc-node-client";
-//import { StargateClient, StargateBearerToken, Query, Response, promisifyStargateClient } from "@stargate-oss/stargate-grpc-node-client";
+import { StargateClient, StargateTableBasedToken, Query, Batch, BatchQuery, Response, promisifyStargateClient } from "@stargate-oss/stargate-grpc-node-client";
+//import { StargateClient, StargateBearerToken, Query, Batch, BatchQuery, Response, promisifyStargateClient } from "@stargate-oss/stargate-grpc-node-client";
 
 try {
+    // Stargate OSS configuration for locally hosted docker image
     const auth_endpoint = "http://localhost:8081/v1/auth";
     const username = "cassandra";
     const password = "cassandra";
-    //const bearer_token = "AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b";
-    const bearer_token = "AstraCS:qTvPRvZTLygyfEZynXIRcOrs:cbd02f55e76e6e948673e8127bbf81feeb6a5564ccfe1f513cf73b9409cf70ed";
-
     const stargate_uri = "localhost:8090";
-    // const astra_uri = "a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com:443";
-    const astra_uri = "ff1d03c6-aaae-440c-b463-4508b2993d0f-us-east1.apps.astra-dev.datastax.com:443";
 
-    // Create a table based auth token Stargate/Cassandra authentication using the default C* username and password
+    // Astra DB configuration
+    //const astra_uri = "0ba933af-90c2-46e9-887a-a387ba75411b-westus2.apps.astra-dev.datastax.com:443";
+    //const bearer_token = "AstraCS:YUskXkABjYztQJfTHNkOGHOI:09fa16889d7217b23b3854d3b2858a17b895ec272494a1539e47200b87567f26";
+
+    // Set up the authenication 
+    // For Stargate OSS: Create a table based auth token Stargate/Cassandra authentication using the default C* username and password
     const credentials = new StargateTableBasedToken({authEndpoint: auth_endpoint, username: username, password: password});
-    // Enter a bearer token for Astra
+    // For Astra DB: Enter a bearer token for Astra, downloaded from the Astra DB dashboard
     // const bearerToken = new StargateBearerToken(bearer_token);
     // const credentials = grpc.credentials.combineChannelCredentials(grpc.credentials.createSsl(), bearerToken);
+    
     // console.log(credentials);
 
-    // Create the gRPC client, passing it the address of the gRPC endpoint
+    // Create the gRPC client
+    // For Stargate OSS: passing it the address of the gRPC endpoint
     const stargateClient = new StargateClient(stargate_uri, grpc.credentials.createInsecure());
+    // For Astra DB: passing the credentials created above
     //const stargateClient = new StargateClient(astra_uri, credentials);
+    
     console.log("made client");
-
 
     // Create a promisified version of the client, so we don't need to use callbacks
     const promisifiedClient = promisifyStargateClient(stargateClient);
+    
     console.log("promisified client")
     
+    // For Stargate OSS: generate authentication metadata that is passed in the executeQuery and executeBatch statements
+    // For Astra DB: comment out
     const authenticationMetadata = await credentials.generateMetadata({service_url: auth_endpoint});
 
-    // Create a new keyspace
+    // For Stargate OSS: Create a new keyspace
+    // For Astra DB: comment out, and create  a keyspace in the Astra DB dashboard
     const createKeyspaceStatement = new Query();
+    // Set the CQL statement
     createKeyspaceStatement.setCql("CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};");
 
     await promisifiedClient.executeQuery(createKeyspaceStatement, authenticationMetadata);
 
-    // Create a new table
+    console.log("created keyspace");
+
+    // For Stargate OSS and Astra DB: Create a new table
     const createTableStatement = new Query();
+    // Set the CQL statement
     createTableStatement.setCql("CREATE TABLE IF NOT EXISTS test.users (key text PRIMARY KEY,value text);");
 
-    await promisifiedClient.executeQuery(createTableStatement, authenticationMetadata);
+    // For Astra DB: delete authenticationMetadata from the following executeQuery statement 
+    await promisifiedClient.executeQuery(
+      createTableStatement,
+      authenticationMetadata
+    );
 
-    // INSERT two rows/records
-    // const insertOne = new BatchQuery();
-    // const insertTwo = new BatchQuery();
+    console.log("created table");
 
-    // insertOne.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Lorina', 'Poland')`);
-    // insertTwo.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Doug', 'Wettlaufer')`);
+    // For Stargate OSS and Astra DB: INSERT two rows/records
+    // Create two queries that will be run in a batch statement
+    const insertOne = new BatchQuery();
+    const insertTwo = new BatchQuery();
 
-    // // Execute the batch
-    // const batch = new Batch();
-    // batch.setQueriesList([insertOne, insertTwo]);
+    // Set the CQL statement
+    insertOne.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Lorina', 'Poland')`);
+    insertTwo.setCql(`INSERT INTO test.users (firstname, lastname) VALUES('Doug', 'Wettlaufer')`);
 
-    // const batchResult = await promisifiedClient.executeBatch(
-    //   batch,
-    //   authenticationMetadata
-    // );
-    // console.log("inserted data");
+    // Define the new batch to include the 2 insertions
+    const batch = new Batch();
+    batch.setQueriesList([insertOne, insertTwo]);
 
-    const queryString = 'SELECT firstname, lastname FROM test.users;'
+    // For Stargate OSS and Astra DB: execute the batch statement
+    // For Astra DB: delete authenticationMetadata from the following executeQuery statement 
+    const batchResult = await promisifiedClient.executeBatch(
+      batch,
+      authenticationMetadata
+    );
+    console.log("inserted data");
+
+    // For Stargate OSS and Astra DB: SELECT the data to read from the table
     const query = new Query();
+    const queryString = 'SELECT firstname, lastname FROM test.users;'
+    // Set the CQL statement using the string defined in the last line
     query.setCql(queryString);
-    //console.log(queryString);
 
+    // For Stargate OSS and Astra DB: execute the query statement
+    // For Astra DB: delete authenticationMetadata from the following executeQuery statement 
     const response = await promisifiedClient.executeQuery(
       query,
       authenticationMetadata
     );
+
     console.log("select executed")
 
+    // Get the results from the execute query statement
+    // and separate into an array to print out the results
     const resultSet = response.getResultSet();
     const rows = resultSet.getRowsList();
 
+    // This for loop gets 2 results
     for ( let i = 0; i < 2; i++) {
       var valueToPrint = "";
       for ( let j = 0; j < 2; j++) {
@@ -84,8 +113,12 @@ try {
         valueToPrint += value;
         valueToPrint += " ";
       }
+      console.log(valueToPrint);
     }
+
+    console.log("everything worked!")
   } catch (e) {
-    // something went wrong
+    // Print out any errors that occur while running this script
+    console.log(e);
   }
 
